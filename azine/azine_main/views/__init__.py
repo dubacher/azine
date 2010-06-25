00a903ddd3ab21305772400ffee1f3369376e216
philomat <philomat@popkultur.net>
2010-06-25 10:52:23 +0200

    Removed temp home view

--- azine/azine_main/views/__init__.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render_to_response 
-from django.template import RequestContext
-
-def tmp_home(request):
-    return render_to_response('html/base.html', 
-        {}, context_instance=RequestContext(request))
