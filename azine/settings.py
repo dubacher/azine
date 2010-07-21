241716fa83ab7d330ac3375021df0a04cf25de36
philomat <philomat@popkultur.net>
2010-07-21 15:33:07 +0200

    Absolute path to sqlite file

--- azine/settings.py
+++ azine/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'azine'             # Or path to database file if using sqlite3.
+DATABASE_NAME = os.path.join(PROJECT_PATH, 'azine')             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
