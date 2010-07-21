2cf904d32ad670ea28d9d2d11acc552386c19dbb
philomat <philomat@popkultur.net>
2010-07-21 15:28:40 +0200

    media/googlehostedservice.html

--- azine/urls.py
+++ azine/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^', include('azine_main.urls')),
 
+    (r'^googlehostedservice\.html$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'path': 'googlehostedservice.html'}),
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 
     url(r'^', include('cms.urls')),
