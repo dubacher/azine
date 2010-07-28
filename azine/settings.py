71591ed0a9e789502539b611579098ab706c4681
Samuel Luescher <sam@sam-macbookpro.local>
2010-07-28 22:23:07 +0200

    Added django-cms-columns submodule

--- azine/settings.py
+++ azine/settings.py
@@ -1,7 +1,6 @@
 # Django settings for azine project.
 import socket, os
-#DEV = socket.gethostname() in ('sam-imac.local', 'your-host-here')
-DEV = True
+DEV = socket.gethostname() in ('sam-imac.local', 'sam-macbookpro.local', 'your-host-here')
 
 DEBUG = DEV
 TEMPLATE_DEBUG = DEBUG
@@ -15,9 +14,9 @@ MANAGERS = ADMINS
 
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = os.path.join(PROJECT_PATH, 'azine')             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'azine'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'root'             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
