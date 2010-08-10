4e3d23cd48932b2ec9dde767a5b351864d418e74
philomat <philomat@popkultur.net>
2010-08-10 16:47:19 +0200

    Renamed views/* in a coherent fashion

--- azine/azine_main/urls.py
+++ azine/azine_main/urls.py
@@ -5,11 +5,11 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
     
-    url(r'^job/(\d+)/apply/$', 'azine_main.views.job_view.apply', name='job_apply'), 
-    url(r'^job/(\d+)/$', 'azine_main.views.job_view.detail', name='job_detail'), 
-    url(r'^job/add/$', 'azine_main.views.job_view.add', name='job_add'),
-    url(r'^job/$', 'azine_main.views.job_view.index', name='job_index'),
-    url(r'^profile/you/applications/$', 'azine_main.views.application_view.index_for_current_user', name='application_index_for_current_user'),
+    url(r'^job/(\d+)/apply/$', 'azine_main.views.job_views.apply', name='job_apply'), 
+    url(r'^job/(\d+)/$', 'azine_main.views.job_views.detail', name='job_detail'), 
+    url(r'^job/add/$', 'azine_main.views.job_views.add', name='job_add'),
+    url(r'^job/$', 'azine_main.views.job_views.index', name='job_index'),
+    url(r'^profile/you/applications/$', 'azine_main.views.application_views.index_for_current_user', name='application_index_for_current_user'),
     url(r'^rss/latest/jobs/$', LatestEntriesFeed()),
     url(r'^invitation/add/$', 'azine_main.views.invitation_views.add', name='invitation_add'),
 
